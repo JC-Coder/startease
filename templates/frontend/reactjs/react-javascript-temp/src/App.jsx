abc3b02d0f4e887d01b224b73af97cfcdc68e69b
xeuxdev <tochukwujohn24@gmail.com>
2023-10-04 14:20:29 +0100

    chore: Update reactjs templates

--- templates/frontend/reactjs/react-javascript-temp/src/App.jsx
+++ templates/frontend/reactjs/react-javascript-temp/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react"
 import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
 import StartEaseLogo from "/startease.svg"
 import "./App.css"
 
@@ -13,14 +12,11 @@ function App() {
         <a href="https://github.com/JC-Coder/startease" target="_blank">
           <img src={StartEaseLogo} className="logo" alt="StartEase logo" />
         </a>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>StartEase + Vite + React</h1>
+      <h1>StartEase + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -30,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the StartEase, Vite and React logos to learn more
+        Click on the StartEase and React logos to learn more
       </p>
     </>
   )
--- templates/frontend/reactjs/react-typescript-temp/src/App.tsx
+++ templates/frontend/reactjs/react-typescript-temp/src/App.tsx
@@ -1,8 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react"
+import reactLogo from "./assets/react.svg"
 import StartEaseLogo from "/startease.svg"
-import './App.css'
+import "./App.css"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,14 +12,11 @@ function App() {
         <a href="https://github.com/JC-Coder/startease" target="_blank">
           <img src={StartEaseLogo} className="logo" alt="StartEase logo" />
         </a>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>StartEase + Vite + React + TS</h1>
+      <h1>StartEase + React + TS</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -30,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the StartEase and React logos to learn more
       </p>
     </>
   )
