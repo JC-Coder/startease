be0d22a0a3363122b58397274f9acb16c23af268
abdullah137 <abdullahyahaya46@gmail.com>
2023-10-09 14:10:41 +0100

    feat: added the typeorm modules, abstract documents and sample entity classes

--- templates/backend/nestjs/base/databases.js
+++ templates/backend/nestjs/base/databases.js
@@ -39,3 +39,55 @@ import { ENVIRONMENT } from 'src/common/configs/environment';
 })
 export class DatabaseModule {}
 `;
+
+export const TypeOrmDatabaseModule = `
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ENVIRONMENT } from 'src/common/configs/environment';
+
+// At your terminal run: docker-compose up to start your database server 
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: ENVIRONMENT.DB.HOST || 'localhost',
+      port: 5435,
+      database: ENVIRONMENT.DB.DATABASE || 'startease',
+      username: ENVIRONMENT.DB.USER || 'root',
+      password: ENVIRONMENT.DB.PASSWORD || '123',
+      autoLoadEntities: true,
+      entities: [__dirname + '/../**/*.entity.{js,ts}'],
+      synchronize: true, // when production set to false
+    })
+  ]
+})
+
+export class DatabaseModule {}
+`;
+
+export const TypeOrmAbstractDocument = `
+import { PrimaryGeneratedColumn } from 'typeorm';
+
+export class AbstractEntity<T> {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  constructor(entity: Partial<T>) {
+    Object.assign(this, entity);
+  }
+}
+`;
+export const TypeOrmEntity = `
+import { Entity, Column } from 'typeorm';
+import { AbstractEntity } from '../../database/abstract.entity';
+
+@Entity()
+export class User extends AbstractEntity<User> {
+
+  @Column('text')
+  email: string;
+
+  @Column('text')
+  password: string;
+}
+`;
